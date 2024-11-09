5dbde99b5e42dbedef7e6b33ea4b4ff0328180ed
yukotakeichi <yuko_murase_aa@mail.toyota.co.jp>
2024-11-09 16:40:05 +0900

    change; database/ parameters table

--- src/db/migrations/20241108124903_create_parameters.js
+++ src/db/migrations/20241108124903_create_parameters.js
@@ -2,30 +2,28 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-const table="parameters"
-exports.up = async(knex) =>{
-    await knex.schema.createTable(table, (table) => {
-        table.increments("id").primary();
-        table.integer("user_id")
-            .references("users.id")
-            .onDelete("CASCADE")
-        table.integer("arg1_min").notNullable()
-        table.integer("arg1_max").notNullable()
-        table.integer("arg1_decimal").notNullable()
-        table.integer("arg2_min").notNullable()
-        table.integer("arg2_max").notNullable()
-        table.integer("arg2_decimal").notNullable()
-        table.string("operator").notNullable()
-        table.integer("res_max").notNullable()
-    });
+const table = "parameters";
+exports.up = async (knex) => {
+  await knex.schema.createTable(table, (table) => {
+    table.increments("id").primary();
+    table.integer("user_id").references("users.id").onDelete("CASCADE");
+    table.integer("arg1_min");
+    table.integer("arg1_max");
+    table.specificType("arg1_list", "decimal[]");
+    table.integer("arg1_decimal");
+    table.integer("arg2_min");
+    table.integer("arg2_max");
+    table.integer("arg2_decimal");
+    table.specificType("arg2_list", "decimal[]");
+    table.string("operator").notNullable();
+    table.integer("res_max").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = async(knex) => {
-    await knex.schema.dropTable(table)
+exports.down = async (knex) => {
+  await knex.schema.dropTable(table);
 };
-
-
--- src/db/seeds/002_parameters.js
+++ src/db/seeds/002_parameters.js
@@ -2,12 +2,38 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-const table="parameters"
-exports.seed = async function(knex) {
+const table = "parameters";
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex(table).del()
+  await knex(table).del();
   await knex(table).insert([
-    {id: 1, user_id:1,arg1_min: 0,arg1_max:10,arg1_decimal:0, arg2_min: 0,arg2_max:10,arg2_decimal:0,operator:"+",res_max:20 },
-    {id: 2, user_id:1,arg1_min: 0,arg1_max:100,arg1_decimal:0, arg2_min: 0,arg2_max:50,arg2_decimal:0,operator:"+",res_max:100 },
+    {
+      id: 1,
+      user_id: 1,
+      arg1_min: 0,
+      arg1_max: 10,
+      arg1_decimal: 0,
+      arg1_list: null,
+      arg2_min: 0,
+      arg2_max: 10,
+      arg2_decimal: 0,
+      arg2_list: null,
+      operator: "+",
+      res_max: 20,
+    },
+    {
+      id: 2,
+      user_id: 1,
+      arg1_min: 0,
+      arg1_max: 100,
+      arg1_decimal: 0,
+      arg1_list: null,
+      arg2_min: 0,
+      arg2_max: 50,
+      arg2_decimal: 0,
+      arg2_list: null,
+      operator: "+",
+      res_max: 100,
+    },
   ]);
 };
