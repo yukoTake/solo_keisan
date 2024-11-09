1f909559d1d56b338c7e004c96431eafa7dc8ffc
yukotakeichi <yuko_murase_aa@mail.toyota.co.jp>
2024-11-09 16:56:19 +0900

    change; database/ result_detail table

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(express.json());
 // app.use("/", express.static(__dirname + "/public"));
 
+//result_summary------------------------------------------
+app.get("/result_summary/:user_id", async (req, res) => {
+  console.log("--index.js--app.get--/result_summary--start-");
+  const userID = req.params.user_id;
+  const resData = await readResultSummery.all(db, userID);
+  console.log("resData;", resData);
+  res.status(200).json(resData);
+});
+
+//result_detail------------------------------------------
 app.get("/result_summary/:user_id", async (req, res) => {
   console.log("--index.js--app.get--/result_summary--start-");
   const userID = req.params.user_id;
--- src/db/migrations/20241108124933_create_result_detail.js
+++ src/db/migrations/20241108124933_create_result_detail.js
@@ -2,27 +2,29 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-const table="result_detail"
-exports.up = async(knex) =>{
-    await knex.schema.createTable(table, (table) => {
-        table.increments("id").primary();
-        table.integer("summary_id")
-            .references("result_summary.id")
-            .onDelete("CASCADE")
-        table.decimal("arg1").notNullable()
-        table.decimal("arg2").notNullable()
-        table.string("operator").notNullable()
-        table.decimal("correct").notNullable()
-        table.decimal("answered").notNullable()
-        table.boolean("isCorrectly").notNullable()
-    });
+const table = "result_detail";
+exports.up = async (knex) => {
+  await knex.schema.createTable(table, (table) => {
+    table
+      .integer("summary_id")
+      .references("result_summary.id")
+      .onDelete("CASCADE")
+      .notNullable();
+    table.integer("question_no").notNullable();
+    table.primary(["summary_id", "question_no"]);
+    table.decimal("arg1").notNullable();
+    table.decimal("arg2").notNullable();
+    table.string("operator").notNullable();
+    table.decimal("correct").notNullable();
+    table.decimal("answered").notNullable();
+    table.boolean("isCorrectly").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = async(knex) => {
-    await knex.schema.dropTable(table)
+exports.down = async (knex) => {
+  await knex.schema.dropTable(table);
 };
-
